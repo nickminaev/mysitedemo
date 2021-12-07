69eaf4c259b1c1874184f290d4dcadee5f06ac07
nickminaev <nickgramru@gmail.com>
2021-12-07 17:23:04 +0200

    Implement MIME types metadata in the deployment script

--- cicd/deployment.py
+++ cicd/deployment.py
@@ -4,10 +4,19 @@ import os
 import boto3
 import sys
 from botocore.exceptions import ClientError
+import mimetypes
 
 ENV_S3_BUCKET_NAME = 'S3_BUCKET_NAME'
 ENV_SITE_CONTENTS_PATH = 'SITE_CONTENTS_PATH'
 site_contents_file_path = Path(os.environ.get(ENV_SITE_CONTENTS_PATH))
+
+
+def get_mimetype(object_path):
+    content_type, encoding = mimetypes.guess_type(object_path)
+    if content_type is None:
+       return 'binary/octet-stream'
+    return content_type
+    
 
 class BucketManager:
 
@@ -16,6 +25,7 @@ class BucketManager:
         self._bucket_name = bucket_name
 
     def upload_files(self, object_path):
+        #https://stackoverflow.com/questions/34550816/aws-content-type-settings-in-s3-using-boto3
         # If the object is taken from the parent directory, then the key is the file name
         bucket_key = object_path.name
         # If the object originates from one of the subdirectories, its key would be the relative path
@@ -25,7 +35,9 @@ class BucketManager:
         try:
             if object_path.is_file():
                 sys.stdout.write(f'Uploading {bucket_key}\n')
-                response = self._s3_client.upload_file(str(object_path), self._bucket_name, bucket_key)
+                content_type = get_mimetype(object_path)
+                response = self._s3_client.upload_file(str(object_path), self._bucket_name, bucket_key, 
+                    ExtraArgs={'ContentType':content_type})
             if object_path.is_dir():
                 for os_obj in object_path.iterdir():
                     self.upload_files(os_obj)
